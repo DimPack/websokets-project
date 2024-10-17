48a729e5385c445b472bb59c3e2158d5276994cf
Dima Prosto <csourgg@gmail.com>
2024-10-17 12:19:32 +0300

    add resetform

--- client/src/components/MessageList/index.jsx
+++ client/src/components/MessageList/index.jsx
@@ -4,27 +4,31 @@ import { getAllMessages } from "../../store/chatSlice";
 import Message from "../Message";
 
 const MessageList = () => {
-    const isMounted = useRef(true);
-    const { messages, error, isPending } = useSelector((store) => store.chat);
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (isMounted.current) {
-            isMounted.current = false;
-            dispatch(getAllMessages());
-        }
-        //eslint-disable-next-line
-    },[]);
-    const showMessage = (msg)=> <Message key={msg._id} msg={msg}/>
-    if (isPending) {
-        return <p>Loading...</p>;
-      }
-      if (error) {
-        return <p>MessageList Error!!! {error}</p>;
-      }
-    return <section> 
-        {messages.length === 0 ? <p>No messages</p> : messages.map(showMessage)}
+  const isMounted = useRef(true);
+  const { messages, error, isPending } = useSelector((store) => store.chat);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (isMounted.current) {
+      isMounted.current = false;
+      dispatch(getAllMessages());
+    }
+    //eslint-disable-next-line
+  }, []);
+  useEffect(() => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+  }, [messages.length]);
+  const showMessage = (msg) => <Message key={msg._id} msg={msg} />;
+  if (isPending) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>MessageList Error!!! {error}</p>;
+  }
+  return (
+    <section>
+      {messages.length === 0 ? <p>No messages</p> : messages.map(showMessage)}
     </section>
-    
-}
+  );
+};
 
 export default MessageList;
